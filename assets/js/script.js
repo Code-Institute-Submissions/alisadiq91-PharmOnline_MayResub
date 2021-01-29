a5719a786db96fc28791d68fa50544c6e02b848f
alisadiq91 <ali.sadiq91@gmail.com>
2021-01-29 20:41:03 +0000

    added hover effect using js to steps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-console.log("helo");
+$(document).ready(function(){
+    $("steps-hover").mouseenter(function(){
+        $(this).html("<p>All you need to do is simply, download our app, register your details and select which medication you would like to order!</p>");
+
+    });
+
+    $("steps-hover").mouseout(function(){
+        $(this).css("color","red");
+
+    });
+
+
+});
+
+
+
+$(function(){
+    $("#steps-hover").hover(function(){
+        $(this).html("<p>All you need to do is simply, download our app, register your details and select which medication you would like to order!</p>");
+
+    }, function(){
+        $(this).css("color","red");
+    });
+});
