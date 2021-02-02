5820e0f3c42d09f180178a71120477dc250e6ca0
alisadiq91 <ali.sadiq91@gmail.com>
2021-02-02 16:38:34 +0000

    added jquery to steps section and added a download app section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,64 @@
-$(document).ready(function(){
-    $("steps-hover").mouseenter(function(){
-        $(this).html("<p>All you need to do is simply, download our app, register your details and select which medication you would like to order!</p>");
-
+ $(document).ready(function () {
+    $(".hover-me-1").mouseover(function () {
+      $("p.show-me-1").show();
+      $("div.placehold-1").hide();
     });
-
-    $("steps-hover").mouseout(function(){
-        $(this).css("color","red");
-
+    $(".hover-me-1, p.show-me-1").mouseleave(function () {
+      $("p.show-me-1").hide();
+       $("div.placehold-1").show();
     });
-
-
-});
-
-
-
-$(function(){
-    $("#steps-hover").hover(function(){
-        $(this).html("<p>All you need to do is simply, download our app, register your details and select which medication you would like to order!</p>");
-
-    }, function(){
-        $(this).css("color","red");
+    $("p.show-me-1").mouseover(function () {
+      $(this).stop(true, true).show();
     });
-});
+  });
+
+
+   $(document).ready(function () {
+    $(".hover-me-2").mouseover(function () {
+      $("p.show-me-2").show();
+      $("div.placehold-2").hide();
+    });
+    $(".hover-me-2, p.show-me-2").mouseleave(function () {
+      $("p.show-me-2").hide();
+       $("div.placehold-2").show();
+    });
+    $("p.show-me-2").mouseover(function () {
+      $(this).stop(true, true).show();
+    });
+  });
+
+  
+  
+   $(document).ready(function () {
+    $(".hover-me-3").mouseover(function () {
+      $("p.show-me-3").show();
+      $("div.placehold-3").hide();
+    });
+    $(".hover-me-3, p.show-me-3").mouseleave(function () {
+      $("p.show-me-3").hide();
+       $("div.placehold-3").show();
+    });
+    $("p.show-me-3").mouseover(function () {
+      $(this).stop(true, true).show();
+    });
+  });
+
+  
+  
+   $(document).ready(function () {
+    $(".hover-me-4").mouseover(function () {
+      $("p.show-me-4").show();
+      $("div.placehold-4").hide();
+    });
+    $(".hover-me-4, p.show-me-4").mouseleave(function () {
+      $("p.show-me-4").hide();
+       $("div.placehold-4").show();
+    });
+    $("p.show-me-4").mouseover(function () {
+      $(this).stop(true, true).show();
+    });
+  });
+
+
+
+
