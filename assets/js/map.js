970b77c06f027174a1ac8b88a8ce8f5b0039edf1
alisadiq91 <ali.sadiq91@gmail.com>
2021-04-25 17:03:37 +0000

    added another 6 pharmacies and a form for the customer to fill out for each branch

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,7 @@
 function initMap (){
 
 var locations = {
-    zoom:8,
+    zoom:6,
     center: {lat:53.8008, lng:-1.5491},
 };
 
@@ -28,6 +28,36 @@ addPharmacy({coordinates:{lat:53.4668,lng:-2.2339},
     content:'<div class="map-click"><h4>Manchester</h4></div>',
     name: "Manchester",
     divid: "manchester"
+});
+addPharmacy({coordinates:{lat:51.5249,lng:-0.1224},
+    content:'<div class="map-click"><h4>London</h4></div>',
+    name: "London",
+    divid: "london"
+});
+addPharmacy({coordinates:{lat:53.3813,lng:-1.4883},
+    content:'<div class="map-click"><h4>Sheffield</h4></div>',
+    name: "Sheffield",
+    divid: "sheffield"
+});
+addPharmacy({coordinates:{lat:52.4507,lng:-1.9305},
+    content:'<div class="map-click"><h4>Birmingham</h4></div>',
+    name: "Birmingham",
+    divid: "birmingham"
+});
+addPharmacy({coordinates:{lat:54.9791,lng:-1.6146},
+    content:'<div class="map-click"><h4>Newcastle</h4></div>',
+    name: "Newcastle",
+    divid: "newcastle"
+});
+addPharmacy({coordinates:{lat:53.4047,lng:-2.9653},
+    content:'<div class="map-click"><h4>Liverpool</h4></div>',
+    name: "Liverpool",
+    divid: "liverpool"
+});
+addPharmacy({coordinates:{lat:52.9397,lng:-1.1928},
+    content:'<div class="map-click"><h4>Nottingham</h4></div>',
+    name: "Nottingham",
+    divid: "nottingham"
 });
 
 
