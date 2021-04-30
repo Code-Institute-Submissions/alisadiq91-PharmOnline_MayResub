c907c1ed54facd590ed0c22d43408de592c173ec
alisadiq91 <ali.sadiq91@gmail.com>
2021-04-30 15:34:15 +0000

    condensed pharmacy list in js file into an array

--- assets/js/map.js
+++ assets/js/map.js
@@ -12,56 +12,22 @@ var map = new google.maps.Map(document.getElementById("map"), locations);
 
 var infoWindow = null;
 
-addPharmacy({coordinates:{lat:53.8013853,lng:-1.5643944},
-    content:'<div class="map-click"><h4>Leeds</h4></div>',
-    name: "Leeds",
-    divid: "leeds"
-});
-addPharmacy({coordinates:{lat:53.9600,lng:-1.0873},
-    content:'<div class="map-click"><h4>York</h4></div>',
-    name: "York",
-    divid: "york"
-});
-addPharmacy({coordinates:{lat:53.7960,lng:-1.7594},
-    content:'<div class="map-click"><h4>Bradford</h4></div>',
-    name: "Bradford",
-    divid: "bradford"
-});
-addPharmacy({coordinates:{lat:53.4668,lng:-2.2339},
-    content:'<div class="map-click"><h4>Manchester</h4></div>',
-    name: "Manchester",
-    divid: "manchester"
-});
-addPharmacy({coordinates:{lat:51.5249,lng:-0.1224},
-    content:'<div class="map-click"><h4>London</h4></div>',
-    name: "London",
-    divid: "london"
-});
-addPharmacy({coordinates:{lat:53.3813,lng:-1.4883},
-    content:'<div class="map-click"><h4>Sheffield</h4></div>',
-    name: "Sheffield",
-    divid: "sheffield"
-});
-addPharmacy({coordinates:{lat:52.4507,lng:-1.9305},
-    content:'<div class="map-click"><h4>Birmingham</h4></div>',
-    name: "Birmingham",
-    divid: "birmingham"
-});
-addPharmacy({coordinates:{lat:54.9791,lng:-1.6146},
-    content:'<div class="map-click"><h4>Newcastle</h4></div>',
-    name: "Newcastle",
-    divid: "newcastle"
-});
-addPharmacy({coordinates:{lat:53.4047,lng:-2.9653},
-    content:'<div class="map-click"><h4>Liverpool</h4></div>',
-    name: "Liverpool",
-    divid: "liverpool"
-});
-addPharmacy({coordinates:{lat:52.9397,lng:-1.1928},
-    content:'<div class="map-click"><h4>Nottingham</h4></div>',
-    name: "Nottingham",
-    divid: "nottingham"
-});
+let pharmacies = [
+{name:"Leeds",divid:"leeds",content:'<div class="map-click"><h4>Leeds</h4></div>',coordinates:{lat:53.8013853,lng:-1.5643944}},
+{name:"Bradford",divid:"bradford",content:'<div class="map-click"><h4>Bradford</h4></div>',coordinates:{lat:53.7960,lng:-1.7594}},
+{name:"Manchester",divid:"manchester",content:'<div class="map-click"><h4>Manchester</h4></div>',coordinates:{lat:53.4668,lng:-2.2339}},
+{name:"Sheffield",divid:"sheffield",content:'<div class="map-click"><h4>Sheffield</h4></div>',coordinates:{lat:53.3813,lng:-1.4883}},
+{name:"York",divid:"york",content:'<div class="map-click"><h4>York</h4></div>',coordinates:{lat:53.9600,lng:-1.0873}},
+{name:"London",divid:"london",content:'<div class="map-click"><h4>London</h4></div>',coordinates:{lat:51.5249,lng:-0.1224}},
+{name:"Birmingham",divid:"birmingham",content:'<div class="map-click"><h4>Birmingham</h4></div>',coordinates:{lat:52.4507,lng:-1.9305}},
+{name:"Newcastle",divid:"newcastle",content:'<div class="map-click"><h4>Newcastle</h4></div>',coordinates:{lat:54.9791,lng:-1.6146}},
+{name:"Nottingham",divid:"nottingham",content:'<div class="map-click"><h4>Nottingham</h4></div>',coordinates:{lat:52.9397,lng:-1.1928}},
+{name:"Liverpool",divid:"liverpool",content:'<div class="map-click"><h4>Liverpool</h4></div>',coordinates:{lat:53.4047,lng:-2.9653}},
+]
+
+for (let i = 0; i < pharmacies.length; i++) {
+    addPharmacy(pharmacies[i]);
+}
 
 //map markers
 
