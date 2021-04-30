e0033b0ad29c896c25f3513604c3de1a8d96cce5
alisadiq91 <ali.sadiq91@gmail.com>
2021-04-30 15:40:19 +0000

    edited carousel mouseover js function to avoid repitition

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,70 +25,25 @@ function closeNav() {
 }
 
 //steps section mouseover 
+
+const numSlides = 4;
+
+$(document).ready(function () {
+    for (let i = 1; i <= numSlides; i++) {
+        $(`.hover-me-${i}`).mouseover(function () {
+            $(`p.show-me-${i}`).show();
+            $(`div.placehold-${i}`).hide();
+        });
+        $(`.hover-me-${i}, p.show-me-${i}`).mouseleave(function () {
+            $(`p.show-me-${i}`).hide();
+            $(`div.placehold-${i}`).show();
+        });
+        $(`p.show-me-${i}`).mouseover(function () {
+            $(this).stop(true, true).show();
+        });
+    }
+});
  
- $(document).ready(function () {
-    $(".hover-me-1").mouseover(function () {
-      $("p.show-me-1").show();
-      $("div.placehold-1").hide();
-    });
-    $(".hover-me-1, p.show-me-1").mouseleave(function () {
-      $("p.show-me-1").hide();
-       $("div.placehold-1").show();
-    });
-    $("p.show-me-1").mouseover(function () {
-      $(this).stop(true, true).show();
-    });
-  });
-
-
-   $(document).ready(function () {
-    $(".hover-me-2").mouseover(function () {
-      $("p.show-me-2").show();
-      $("div.placehold-2").hide();
-    });
-    $(".hover-me-2, p.show-me-2").mouseleave(function () {
-      $("p.show-me-2").hide();
-       $("div.placehold-2").show();
-    });
-    $("p.show-me-2").mouseover(function () {
-      $(this).stop(true, true).show();
-    });
-  });
-
-  
-  
-   $(document).ready(function () {
-    $(".hover-me-3").mouseover(function () {
-      $("p.show-me-3").show();
-      $("div.placehold-3").hide();
-    });
-    $(".hover-me-3, p.show-me-3").mouseleave(function () {
-      $("p.show-me-3").hide();
-       $("div.placehold-3").show();
-    });
-    $("p.show-me-3").mouseover(function () {
-      $(this).stop(true, true).show();
-    });
-  });
-
-  
-  
-   $(document).ready(function () {
-    $(".hover-me-4").mouseover(function () {
-      $("p.show-me-4").show();
-      $("div.placehold-4").hide();
-    });
-    $(".hover-me-4, p.show-me-4").mouseleave(function () {
-      $("p.show-me-4").hide();
-       $("div.placehold-4").show();
-    });
-    $("p.show-me-4").mouseover(function () {
-      $(this).stop(true, true).show();
-    });
-  });
-
-
-
 //faqs section toggle
 
   var action = 'click';
