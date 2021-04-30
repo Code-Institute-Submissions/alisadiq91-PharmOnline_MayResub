51d4d4d77bde5ac1388fa1aeb0ace0b5d4fecb63
alisadiq91 <ali.sadiq91@gmail.com>
2021-04-30 13:09:17 +0000

    altered the hamburger menu to a side menu for design purposes

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,6 +61,16 @@
     });
   });
 
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
 //faqs section toggle
 
   var action = 'click';
